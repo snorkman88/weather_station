eb86cd27c4619b272a26a59826166c1c461fc961
Milton Eduardo Sosa <milton.eduardo.sosa@gmail.com, milton@switch-ev.com>
2024-04-08 00:55:49 +0100

    Add another handle to get last reading

--- weather_station/src/main.rs
+++ weather_station/src/main.rs
@@ -4,7 +4,10 @@
 //! cargo watch -x run
 //! ```
 
+use std::string;
+
 use axum::http::{Response, StatusCode};
+use axum::Extension;
 use axum::{routing::get, routing::post, Router};
 use serde::{Deserialize, Serialize};
 use tokio::sync::{mpsc, oneshot};
@@ -16,10 +19,13 @@ pub struct Reading {
 
 #[tokio::main]
 async fn main() {
+    let db_actor_handle = DbActorHandle::new();
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
-        .route("/new", post(handle_new_reading));
+        .route("/new", post(handle_new_reading))
+        .route("/last", get(fetch_last_reading))
+        .layer(Extension(db_actor_handle.clone()));
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -37,21 +43,46 @@ async fn root() -> Response<String> {
     Response::new("hola".to_string())
 }
 
-async fn handle_new_reading(new_reading: axum::extract::Json<Reading>) {
-    // "axum::extract::Json<Reading>" extractS the JSON content present int the body of the request
+async fn handle_new_reading(
+    Extension(db_actor_handle): Extension<DbActorHandle>,
+    new_reading: axum::extract::Json<Reading>,
+) {
+    // "axum::extract::Json<Reading>" extractS the JSON content present in the body of the request
     // and converts it to an instance of 'Reading'
-    println!("New reading received from node{}", new_reading.value);
-    //TODO: send this to the DB actor in charge of saving in sqlite
+    println!("New reading received from node {}", new_reading.value);
+
+    //Use the DbActorHandle
+    db_actor_handle
+        .save_new_reading(Reading {
+            value: new_reading.value,
+        })
+        .await;
+}
+
+async fn fetch_last_reading(Extension(db_actor_handle): Extension<DbActorHandle>) -> Response<String>{
+    //Use the DbActorHandle to request DbActor the last reading
+    let last_reading = db_actor_handle.get_last_reading().await;
+    let last_reading_as_json = serde_json::to_string(&last_reading);
+    let response = match last_reading_as_json {
+        Ok(last_reading) => {
+            Response::new(last_reading)
+        },
+        Err(_) => {
+            Response::new("Error".to_string())
+        }
+        
+    };
+    response
 }
 
 //---------------------DB ACTOR-------------------
 
-// DB actor
 struct DbActor {
     inbox: mpsc::Receiver<DbActorMessage>,
 }
 
 // Define the type of messages we can send to the actor
+#[derive(Debug)]
 enum DbActorMessage {
     SaveNewReading {
         respond_to: oneshot::Sender<bool>,
@@ -66,29 +97,34 @@ impl DbActor {
     fn new(inbox: mpsc::Receiver<DbActorMessage>) -> Self {
         DbActor { inbox: inbox }
     }
-    fn handle_db_actor_inbox_msg(&mut self, incoming_msg: DbActorMessage) {
+    async fn process_db_actor_inbox_msg(&mut self, incoming_msg: DbActorMessage) {
         match incoming_msg {
             DbActorMessage::SaveNewReading {
                 respond_to,
                 new_reading,
             } => {
-                // TODO: pass new reading to DbActorHandle instance to write to DB
-                // and return a boolean indicating the status of the operation
+                println!("DbActor.process_db_actor_inbox_msg received a new message ");
+                // Add logic to interact with SQL database here
                 let result: bool = true;
-                let _ = respond_to.send(result);
+                let _ = respond_to.send(result); //Send the result of the operation back to the handle
             }
-            DbActorMessage::GetLastReading { respond_to } => todo!(),
+            DbActorMessage::GetLastReading { respond_to } => {
+                println!("DbActor.get_last_reading received a new message");
+                // Add logic to interact with SQL database here
+                let last_reading = Reading { value: 37.1_f64 };
+                let _ = respond_to.send(last_reading); //Send the result of the operation back to the handle
+            }
         }
     }
 }
 
 async fn run_db_actor(mut actor: DbActor) {
     while let Some(msg) = actor.inbox.recv().await {
-        actor.handle_db_actor_inbox_msg(msg);
+        actor.process_db_actor_inbox_msg(msg).await;
     }
 }
 
-// Handle to communicate with the DbActor
+// ----------------Handle used to communicate with the DbActor---------------
 #[derive(Clone)]
 pub struct DbActorHandle {
     sender: mpsc::Sender<DbActorMessage>,
@@ -98,7 +134,7 @@ impl DbActorHandle {
     pub fn new() -> Self {
         let (sender_to_db_actor, receiver_at_db_actor) = mpsc::channel(10);
         let db_actor = DbActor::new(receiver_at_db_actor);
-        tokio::spawn(run_db_actor(db_actor));
+        tokio::spawn(run_db_actor(db_actor)); //This will run the whole DbActor in the background
         Self {
             sender: sender_to_db_actor,
         }
@@ -116,6 +152,20 @@ impl DbActorHandle {
         // recv.await below. There's no reason to check the
         // failure twice.
         let _ = self.sender.send(msg).await;
-        recv.await.expect("Actor task has been killed");
+        let result = recv.await.expect("Actor task has been killed");
+        println!("The DbActor returned {:?} to the handle", result)
+    }
+
+    pub async fn get_last_reading(self) -> Reading {
+        println!("Requesting last reading to DbActor");
+        let (send, recv) = oneshot::channel();
+        let msg = DbActorMessage::GetLastReading { respond_to: send };
+
+        // Ignore send errors. If this send fails, so does the
+        // recv.await below. There's no reason to check the
+        // failure twice.
+        let _ = self.sender.send(msg).await;
+        let result = recv.await.expect("Actor task has been killed");
+        result
     }
 }
